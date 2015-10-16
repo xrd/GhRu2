207f5ca2d5b433285f509e8086629551ec330ab7
Chris Dawson <xrdawson@gmail.com>
2015-10-16 15:42:18 -0700

    Got working version of espresso test

--- app/src/androidTest/java/com/ghru/MainActivityTest.java
+++ app/src/androidTest/java/com/ghru/MainActivityTest.java
@@ -7,7 +7,11 @@ import org.hamcrest.Matchers.*;
 import org.junit.Before;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -16,16 +20,30 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private MainActivity mainActivity;
-
     public MainActivityTest() {
         super( MainActivity.class );
     }
 
     public void testLoaded() {
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        mainActivity = getActivity();
-        onView( withId( R.id.login_status ))
-                .check(matches(withText("Hello World!")));
+        MainActivity mainActivity = getActivity();
+//           onView( withId( R.id.submit ))
+//                .check(doesNotExist());
+        onView( withId( R.id.username ))
+                .perform(typeText("BurningOnUp"));
+        String password = mainActivity.getString(R.string.github_helper_password);
+        onView(withId(R.id.password))
+                        .perform(typeText(password));
+        onView(withId(R.id.login))
+                .perform(click());
+//        try {
+//            Thread.sleep(3000);
+//        }
+//        catch( Exception e ) {
+//
+//        }
+//
+        onView( withId( R.id.submit ))
+                .check( matches(isDisplayed()) );
     }
 }
