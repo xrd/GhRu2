b7f3cba78a93ea1831ad204d8d3389ef15ee3469
Chris Dawson <xrdawson@gmail.com>
2015-10-16 15:55:15 -0700

    Use the label, not a button

--- app/src/androidTest/java/com/ghru/MainActivityTest.java
+++ app/src/androidTest/java/com/ghru/MainActivityTest.java
@@ -2,18 +2,10 @@ package com.ghru;
 
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
-import org.hamcrest.Matchers.*;
-
-import org.junit.Before;
-
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 /**
  * Created by xrdawson on 10/16/15.
@@ -27,8 +19,6 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     public void testLoaded() {
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         MainActivity mainActivity = getActivity();
-//           onView( withId( R.id.submit ))
-//                .check(doesNotExist());
         onView( withId( R.id.username ))
                 .perform(typeText("BurningOnUp"));
         String password = mainActivity.getString(R.string.github_helper_password);
@@ -36,14 +26,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
                         .perform(typeText(password));
         onView(withId(R.id.login))
                 .perform(click());
-//        try {
-//            Thread.sleep(3000);
-//        }
-//        catch( Exception e ) {
-//
-//        }
-//
-        onView( withId( R.id.submit ))
-                .check( matches(isDisplayed()) );
+        onView( withId( R.id.loggedIn ))
+                .check( matches( withText("Logged into GitHub")) );
     }
 }
